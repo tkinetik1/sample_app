2c23670aa5280c1f48638992adf2c91befe99a4b
tkinetik_lts <tkinetik1@ubuntu.(none)>
2014-03-18 20:41:07 -0700

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,21 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+    def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])    # Not the final implementation!
+    if @user.save
+      sign_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
   def edit
   end
 
@@ -26,30 +41,12 @@ class UsersController < ApplicationController
 
   def destroy
     User.find(params[:id]).destroy
-    flash[:success] = "User destroyed."
+    flash[:success] = "User destroyed"
     redirect_to users_url
   end
 
-  def new
-  	@user = User.new
-  end
-
-  def create
-    @user = User.new(params[:user])    # Not the final implementation!
-    if @user.save
-      sign_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
-    else
-      render 'new'
-    end
-  end
 
   private
-
-    def admin_user
-      redirect_to(root_url) unless current_user.admin?
-    end
 
     def signed_in_user
       unless signed_in?
@@ -61,5 +58,9 @@ class UsersController < ApplicationController
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,10 +15,21 @@ describe User do
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:remember_token) }
+  it { should respond_to(:admin) }
   it { should respond_to(:authenticate) }  
 
   it {should be_valid }
+  it {should_not be_admin }
 
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin }
+  end
+  
   describe "when user is not present" do
   	before { @user.name = "" }
   	it { should_not be_valid }
